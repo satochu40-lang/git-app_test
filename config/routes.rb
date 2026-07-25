df46b4bd129b9e58abbe85ecfd26a69e0e07fd19
satochu40-lang <satochu.40@gmail.com>
2026-07-26 02:44:05 +0900

    Revert "コントローラー作成"
    This reverts commit c88ae4a3f129e4cf62f8dec1842944d6ad83275b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,6 +6,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-   root "posts#index"
-   resources :posts, only: [:index]
+  # root "posts#index"
 end
